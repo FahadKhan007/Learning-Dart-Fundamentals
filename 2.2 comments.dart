e6f0292ae436ba4bc196f2c352ffa784c045d220
Abdullah Al Fahad <52467937+FahadKhan007@users.noreply.github.com>
2023-02-15 19:26:43 +0600

    Section-2

---
+++ 2.2 comments.dart
@@ -0,0 +1,22 @@
+// Section 2 
+// Comments
+
+
+/*
+ This comment syntax is highly discouraged
+ So never use this syntax
+*/
+
+void main()
+{
+  // This is my first line of code
+  print("Hello World"); 
+  print("This is my first application"); // This is another line
+  print(17);
+  
+  // Performing arithmetic operations
+  print(25-17);
+  
+  //Printing out boolean value
+  print(false);
+}
