7d30f70932fe1caae8c42000ebb3cc6590ff1d83
Abdullah Al Fahad <52467937+FahadKhan007@users.noreply.github.com>
2023-02-17 00:04:44 +0600

    Section-5

---
+++ 5.1 Iterators.dart
@@ -0,0 +1,20 @@
+// Section 5
+// iterators
+
+void main()
+{
+  // for loop
+  // while loop
+  // do while loop
+  
+  // loop structure
+    //counter variable --> i
+    // condition check
+    // increment/deccrement
+  
+  // loops --> definite(number of iteration is known) & indefinite(number of iteration is unknown)
+  // definite --> for loop
+  // indefinite --> while loop & do while loop
+    
+}
+
