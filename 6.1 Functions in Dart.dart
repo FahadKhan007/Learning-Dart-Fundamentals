b24b579eeac0b7f7a82c80b641f3375f899678d5
Abdullah Al Fahad <52467937+FahadKhan007@users.noreply.github.com>
2023-02-18 23:30:03 +0600

    Section-6

---
+++ 6.1 Functions in Dart.dart
@@ -0,0 +1,21 @@
+// Section 6
+// Define Functions in Dart
+
+// collection of statements grouped together to perform an operation
+// every func returns a value but if no return value specified then by default it returns null
+
+
+// functions in dart are Objects
+// i.e. functions can be assigned to a variable or passed as a parameter to other functions
+
+
+// returnType(optional) functionName (requiredParameters)
+//  {
+      //  funcetion Body: put the code here 
+      //  return value;
+//  }
+
+void main()
+{
+    
+}
