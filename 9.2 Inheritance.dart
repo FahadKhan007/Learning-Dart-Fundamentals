348eec9f512802274b2ca43a7a08442703d60213
Abdullah Al Fahad <52467937+FahadKhan007@users.noreply.github.com>
2023-02-20 20:17:28 +0600

    Section-9

---
+++ 9.2 Inheritance.dart
@@ -0,0 +1,18 @@
+// Section 9
+
+// Inheritance --> one class inherits the property of another class
+
+// all the class in dart inherits from a common class 'Object'
+// and it provides default implementation of,
+  // toString()
+  // hashCode Getter method 
+  // operator ==  (to compare two objects) 
+
+// parent or base or super class 
+// sub or child class
+
+// 3 types of inheritance
+  // 1. single inheritance  (A-->B)
+  // 2. multi-level inheritance  (A-->B-->C)
+  // 3. hierarchical inheritance  (A-->B) and (A-->C)
+
